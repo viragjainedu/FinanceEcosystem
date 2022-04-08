2deadf96168689ad5394aadf44fc65f51cc718db
viragjainedu <virag.j@somaiya.edu>
2022-04-08 15:11:01 +0530

    Nothing

--- backend/routes/balance.js
+++ backend/routes/balance.js
@@ -1,8 +1,35 @@
 var express = require("express");
 var router = express.Router();
+var nodemailer = require('nodemailer');
+
 
 router.get("/", function(req, res, next) {
-    res.send("This statement is generate by Balance API backend");
+
+    
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'virag.j@somaiya.edu',
+          pass: 'dontopenthis12345'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'virag.j@somaiya.edu',
+        to: 'keval.dk@somaiya.edu',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy fucker!'
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+      res.send({"Email": "Sent"})
+
 });
 
 module.exports = router;
