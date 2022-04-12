35ff6e835b56464736c5082f2c4df16ca23d07d7
Keval2807 <60359960+Keval2807@users.noreply.github.com>
2022-04-12 16:07:03 +0530

    Blogs and News Done

--- backend/routes/blogs.js
+++ backend/routes/blogs.js
@@ -12,9 +12,9 @@ router.get("/", function(req, res, next) {
                 console.log(err);
             }
             // console.log(result);
-            res.send({"result":result})
+            res.send(result)
         }
-      );    
+      );     
 
 });
 
